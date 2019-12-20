76336f334522b38cf2dd436744b13d0289f7e907
Anthony Rentsch <anthony.rentsch@gmail.com>
2019-12-19 19:16:47 -0500

    added epistemic experiment to report

--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+# +
 def load_quadratic_data(num=1500, scale=2, test_size=1/6, sort=True):
     ''' Loads the data for our experiments
 
@@ -39,3 +40,26 @@ def load_quadratic_data(num=1500, scale=2, test_size=1/6, sort=True):
         X_test, y_test = X_test[inds_test], y_test[inds_test]
 
     return X, X_test, y, y_test
+
+def load_sin_data():
+    '''Loads data for epistemic uncertainty experiment.
+    
+    Returns
+    -------
+    X : np.array
+    X_test : np.array
+    y : np.array
+    y_test : np.array
+    '''
+    sigma = 0.4
+    f = lambda x: np.sin(0.05*x**2) + np.random.normal(0, sigma, size=len(x))
+    X = np.concatenate([np.linspace(-10, -5, 4),
+                        np.linspace(-5, 5, 500),
+                        np.linspace(5, 10, 4)])
+
+    y = f(X)
+
+    X_test = np.linspace(-10, 10, 200)
+    y_test = f(X_test)
+    
+    return X, y, X_test, y_test
